2dbece890459066b37657f36c7876be1450c0fa6
Mayara Nogueira <151465291+nogueiramayara@users.noreply.github.com>
2023-11-21 19:02:51 -0300

    Opção de saída de sistema e tela extra de exclusão
    Foi inclusa uma tela extra para confirmação dos dados do usuário antes da exclusão do mesmo e uma opção nova no menu que opta pela saída do sistema.
    *Cartorio_MayaraNogueira_20231119_01*

--- Cartorio_MayaraNogueira.cpp
+++ Cartorio_MayaraNogueira.cpp
@@ -4,7 +4,7 @@
 #include <string.h> //biblioteca responsável por cuidar das string
 
 int registro()// função responsável por cadastrar os usuários no sistema
-{	
+{
 	//inicio criação de variaveis/string
 	char arquivo [40];
 	char cpf[40];
@@ -12,7 +12,7 @@ int registro()// função responsável por cadastrar os usuários no sistema
 	char sobrenome[40];
 	char cargo[40];
 	//fim criação de variáveis
-	
+
 	printf("Digite o CPF a ser cadastrado: ");
 	scanf("%s", cpf);
 	
@@ -51,7 +51,7 @@ int registro()// função responsável por cadastrar os usuários no sistema
 	fprintf(file, "\n");
 	fclose(file);//fecha e salva
 
-	printf("Digite o cargo a ser cadastrado: ");//coletando infos do usuário
+	printf("Digite o cargo a ser cadastrado: ");//coletando infos do usuario
 	scanf("%s", cargo);//%s refere-se a string, salva na variável cargo
 	
 	file= fopen(arquivo,"a");//abre arquivo para atualizar
@@ -86,9 +86,9 @@ int consulta()
 		printf("\nEssas são as informações do usuário:\n");
 	}
 	
-	while(fgets(conteudo, 200, file) != NULL)//looping!fgets vai buscar la no arquivo, busca na variavel conteudo, 200 é o tamanho máximo do conteudo, file é o aquivo onde está o conteudo, depois que não achar mais caracteres (ou seja, diferente de nulo, ele para de buscar)
+	while(fgets(conteudo, 200, file) != NULL)//looping!fgets vai buscar la no arquivo, busca na variavel conteudo, 200 é o tamanho máximo do conteudo, file é o aquivo onde está o conteudo, depois que não achar mais caracteres (ou seja, diferente de nulo, ele para de buscar
 	{
-		printf("%s", conteudo);	
+		printf("%s", conteudo);
 	}
 	
 	fclose(file);
@@ -98,11 +98,10 @@ int consulta()
 int deletar()
 {
 	char cpf[40];
+	int opcao=0;
 	
 	printf("Digite o CPF do usuário que deseja deletar: \n");
 	scanf("%s", cpf);
-	
-	remove(cpf);//essa função já existe nas nossas bibliotecas, por isso aqui apenas chamamos um função que já está importada
 	
 	FILE *file;
 	file = fopen(cpf,"r");
@@ -110,9 +109,39 @@ int deletar()
 	
 	if(file == NULL)
 	{
-		printf("O usuário não se encontra no sistema!\n");
+		printf("O usuário não se encontra no sistema!\n\n");
 		system("pause");
 	}
+	
+	else
+	{
+		{
+		FILE *file;
+		file = fopen(cpf,"r");
+		printf("\nEssas são as informações do usuário a ser deletado:\n");
+			
+		while(fgets(cpf,40, file) !=NULL)
+			{
+				printf("%s", cpf);
+			}
+		fclose(file);
+		}
+		
+		printf("Digite novamente o cpf do usuário para confirmar a ação ou 0 para cancelar:\n");
+		scanf("%s",cpf);
+		remove(cpf);//essa função já existe nas nossas bibliotecas, por isso aqui apenas chamamos um função que já está importada
+		
+		FILE *file;
+		file = fopen(cpf,"r");
+		fclose(file);
+		
+		system("cls");
+		
+		printf("Ação concluída!\n\n");
+		system("pause");
+	
+	}
+		
 }
 
 int main() //funcao princiapal
@@ -131,7 +160,8 @@ int main() //funcao princiapal
 		printf("Escolha a opção desejada do menu: \n\n");
 		printf("\t1- Registrar nomes\n");
 		printf("\t2- Consultar nomes\n");
-		printf("\t3- Deletar nomes\n\n");
+		printf("\t3- Deletar nomes\n");
+		printf("\t4- Sair do sistema\n\n");
 		printf("Opção: "); //fim do menu
 	
 		scanf("%d", &opcao); //scanf é responsável por escanear a resposta do usuário e %d por armazenar a variável inteira em opção
@@ -151,6 +181,11 @@ int main() //funcao princiapal
 			
 			case 3:
 			deletar();//chamada de funções
+			break;
+			
+			case 4:
+			printf("Obrigada por utilizar o sistema!\n\n");
+			return 0;//retorna ao valor 0, quebra laço, função, tudo! Ou seja, sai do sistema.
 			break;
 			
 			default: //o default funciona como se fosse um else
